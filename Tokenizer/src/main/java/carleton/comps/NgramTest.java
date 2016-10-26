11846fc5cceedac546e4d314cd80fa73dd367882
jamie <emeryj@carleton.edu>
2016-10-26 13:12:18 -0500

    some readme stuff

--- Tokenizer/src/main/java/carleton/comps/NgramTest.java
+++ Tokenizer/src/main/java/carleton/comps/NgramTest.java
@@ -14,19 +14,24 @@ public class NgramTest {
         try {
             List<Token> c1 = new TokenizerBuilder
                     ("./Tokenizer/src/main/java/carleton/comps/javaparser" +
-                    "/examples/HelloWorld.java","file").getTokens();
+                    "/examples/HelloWorld.java","File").getTokens();
             List<Token> c2 = new TokenizerBuilder
                     ("./Tokenizer/src/main/java/carleton/comps/javaparser" +
-                            "/examples/HelloWorld.java","file").betweenLines
-                    (1,7);
+                            "/examples/HelloWorld.java","File").betweenLines
+                    (1,5);
             System.out.println(c1.size() + " " + c1);
             System.out.println(c2.size() + " " + c2);
 
-            System.out.println(NgramScorer.scoreSimilarity("public static " +
-                    "void main(String[] args) {", "public static void main" +
-                    "(String[] args) { try {"));
+            System.out.println("Ngram Similarity of c1 and c2 is: " +
+                    NgramScorer.scoreSimilarity(c1,c2,5));
 
-            System.out.println(LevScorer.scoreSimilarity(c1,c2));
+            System.out.println("Ngram Similarity of the strings is: " +
+                    NgramScorer.scoreSimilarity("public static " +
+                            "void main(String[] args) {", "public static void main" +
+                            "(String[] args) { try {"));
+
+            System.out.println("c1 and c2 lev similarity is: " + LevScorer
+                    .scoreSimilarity(c1,c2));
 
         } catch (IOException e) {
             e.printStackTrace();
